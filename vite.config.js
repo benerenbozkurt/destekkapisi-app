b5700a934c61980f0a5ae02f0b86b7230d055588
WebDestek <info@destekkapisi.app>
2026-03-19 14:25:08 +0300

    Configure Vite to build all HTML entries

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        article1: 'kumar-bagimliligindan-kurtulmanin-5-adimi.html',
+        privacy: 'privacy-policy.html'
+      }
+    }
+  }
 })
